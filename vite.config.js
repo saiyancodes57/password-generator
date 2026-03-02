4f142f8593651214583051d14e5ea7957ce3e2c7
saiyancodes57 <256424803+saiyancodes57@users.noreply.github.com>
2026-03-02 20:29:45 +0000

    Adding dev variable for devtools

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,15 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
     vue(),
-    vueDevTools(),
-  ],
+    // Only run devtools if we aren't building for production
+    mode === 'development' && vueDevTools(),
+  ].filter(Boolean), // This removes 'false' from the array if in production
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+}))
